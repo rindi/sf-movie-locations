4596ee068cdca46210340b3953a3c28e4c9186e1
rindi <rindi@mail.sfsu.edu>
2016-08-27 18:39:13 -0700

    Added HTML templates to view

--- locations/views.py
+++ locations/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from locations.models import Location, Movie
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     movie_list = Movie.objects.order_by('title')
-    output = '<li>'.join([m.title for m in movie_list])
-    return HttpResponse(output)
+    template = loader.get_template('locations/index.html')    
+    context = {
+        'movie_list': movie_list,
+    }
+    return render(request, 'locations/index.html', context)
+
+    # return HttpResponse(template.render(context, request))
+
+    # output = '<li>'.join([m.title for m in movie_list])
+    # return HttpResponse(output)
