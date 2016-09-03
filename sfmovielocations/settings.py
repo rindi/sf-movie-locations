ad2ac99ef553de17b626ca5f3d791b9aa247127a
rindi <rindi@mail.sfsu.edu>
2016-09-02 23:38:42 -0700

    Changed DB params

--- sfmovielocations/settings.py
+++ sfmovielocations/settings.py
@@ -75,30 +75,18 @@ WSGI_APPLICATION = 'sfmovielocations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
 
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.postgresql',
-#     #     'NAME': 'movielocations',
-#     #     'USER': 'rushabindi',
-#     #     'PASSWORD': 'password',
-#     #     'HOST': 'localhost',
-#     #     'PORT': '5432',
-#     # },
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'serene-retreat-88981',
-#         'USER': 'ldazpwgyltajgi',
-#         'PASSWORD': 'DVBBqfdtYWNQtQQfoTPq4HoDOt',
-#         'HOST': 'ec2-54-225-72-148.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     # }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'movielocations',
+    #     'USER': 'rushabindi',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
+    'default': dj_database_url.config()
+}
 
 
 # Password validation
